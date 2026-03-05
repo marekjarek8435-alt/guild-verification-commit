53c6deca680b4d3178f87422bd5548d911fd487e
marekjarek8435-alt <marekjarek8435@gmail.com>
2026-03-05 21:24:43 +0100

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:23:32.361Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T20:24:31.668Z
+Commit: Patch security vulnerability in authentication
