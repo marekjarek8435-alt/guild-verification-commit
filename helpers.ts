25d55dac65db3d4a4cca17910080e75cf363a0c0
marekjarek8435-alt <marekjarek8435@gmail.com>
2026-03-05 21:23:31 +0100

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:23:12.689Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T20:23:19.682Z
+Commit: Resolve null pointer exception in user service
