01bb401c3b98b5a126baa67c6103fb9b7301af24
marekjarek8435-alt <marekjarek8435@gmail.com>
2026-03-05 21:25:00 +0100

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:24:31.668Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T20:24:49.630Z
+Commit: Add multi-language support
